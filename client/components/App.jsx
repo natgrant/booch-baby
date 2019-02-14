c337f5233b02f41bb46ef22f8238b5b068787085
Nat Grant <email.natgrant@gmail.com>
2019-02-14 19:13:33 +1300

    boilerplate complete!

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+import Header from "./Header";
 
 const App = props => {
-  return <div className="app" />;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return;
+  return <h1>Booch Baby</h1>;
 };
 
 export default Header;
