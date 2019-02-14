535bf2e04bb6697850e6adf16daf2884acf00609
Nat Grant <email.natgrant@gmail.com>
2019-02-15 10:17:32 +1300

    adding combined reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,3 +1,12 @@
 import { combineReducers } from "redux";
 
-export default combineReducers({});
+// Import Reducers to export combined below
+import currentPage from "./current-page";
+// import booch from "./booch";
+// import cart from "./cart";
+
+export default combineReducers({
+  currentPage
+  // booch,
+  // cart
+});
