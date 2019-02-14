ab38ef35bbadf6cd74ab57e039cb19d7a0e23b07
Nat Grant <email.natgrant@gmail.com>
2019-02-15 12:50:09 +1300

    updating reducer with correct data

--- client/components/BoochList.jsx
+++ client/components/BoochList.jsx
@@ -8,37 +8,12 @@ class BoochList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      kombucha: [
-        {
-          id: 1,
-          name: "Coffee Kombucha",
-          description:
-            "An experimental blend of kombucha and coffee, using Peoples Coffee Beans",
-          size: "500 ml",
-          price: 5.99,
-          image: ""
-        },
-        {
-          id: 2,
-          name: "Blueberry and Ginger",
-          description: "Kombucha with organic blueberry and ginger",
-          size: "500 ml",
-          price: 5.99,
-          image: ""
-        },
-        {
-          id: 3,
-          name: "Blueberry and Ginger",
-          description: "Kombucha with organic blueberry and ginger",
-          size: "1 litre",
-          price: 9.99,
-          image: ""
-        }
-      ]
+      kombucha: props.booch
     };
   }
 
   render() {
+    console.log(this.state.kombucha);
     return (
       <Fragment>
         {this.state.kombucha.map((item, idx) => {
@@ -64,6 +39,7 @@ class BoochList extends Component {
 }
 
 const mapStateToProps = state => {
+  console.log(state);
   return {
     booch: state.booch
   };
