b30bff45ac7029b6f850166f6ac538eb1eefe406
Nat Grant <email.natgrant@gmail.com>
2019-02-15 15:58:50 +1300

    working

--- client/components/BoochList.jsx
+++ client/components/BoochList.jsx
@@ -1,31 +1,60 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 
 import { addToCart, navigate } from "../actions";
 
 import BoochListItem from "./BoochListItem";
 
-
 const BoochList = props => {
+  //   return (
+  //     <div>
+  //       <p>
+  //         Welcome! Please select from our delicious selection and don't hesitate
+  //         to let us know if we can answer any of your questions.
+  //       </p>
+  //       {props.booch.map(item => {
+  //         return (
+  //           <BoochListItem
+  //             key={item.id}
+  //             item={item}
+  //             addToCart={props.addToCart}
+  //           />
+  //         );
+  //       })}
+  //     </div>
+  //   );
+  // };
+
   return (
-    <div>
-      <p>
-        Welcome! Please select from our delicious selection and don't hesitate
-        to let us know if we can answer any of your questions.
-      </p>
-      {props.booch.map(item => {
-        return (
-          <BoochListItem
-            key={item.id}
-            item={item}
-            addToCart={props.addToCart}
-          />
-        );
+    <Fragment>
+      {props.booch.map((item, idx) => {
+        if ((idx - 1) % 3 == 0) {
+          return (
+            <Fragment>
+              {/* <div className="columns"> */}
+              <BoochListItem
+                key={item.id}
+                item={item}
+                addToCart={props.addToCart}
+              />
+            </Fragment>
+          );
+        } else if (idx % 3 == 0) {
+          return (
+            <Fragment>
+              <BoochListItem
+                key={item.id}
+                item={item}
+                addToCart={props.addToCart}
+              />
+              {/* </div> */}
+            </Fragment>
+          );
+        }
       })}
-    </div>
+    </Fragment>
   );
 };
-
 
 const mapStateToProps = state => {
   return {
